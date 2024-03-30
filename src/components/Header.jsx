156792cb188362f81360ff2347da3b1358aacb75
Sahand Ghavidel <sahand.gha@gmail.com>
2024-03-30 08:27:21 +0700

    add upload image functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,66 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { signIn, useSession, signOut } from 'next-auth/react';
 import Modal from 'react-modal';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { IoMdAddCircleOutline } from 'react-icons/io';
 import { HiCamera } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
+import { app } from '@/firebase';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
 
 export default function Header() {
   const { data: session } = useSession();
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const filePickerRef = useRef(null);
+  function addImageToPost(e) {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  }
+
+  useEffect(() => {
+    if (selectedFile) {
+      uploadImageToStorage();
+    }
+  }, [selectedFile]);
+
+  async function uploadImageToStorage() {
+    setImageFileUploading(true);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + '-' + selectedFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, selectedFile);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+      },
+      (error) => {
+        console.error(error);
+        setImageFileUploading(false);
+        setImageFileUrl(null);
+        setSelectedFile(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+          setImageFileUploading(false);
+        });
+      }
+    );
+  }
   return (
     <div className='shadow-sm border-b sticky top-0 bg-white z-30 p-3'>
       <div className='flex justify-between items-center max-w-6xl mx-auto'>
@@ -75,7 +127,28 @@ export default function Header() {
           ariaHideApp={false}
         >
           <div className='flex flex-col justify-center items-center h-[100%]'>
-            <HiCamera className='text-5xl text-gray-400 cursor-pointer' />
+            {selectedFile ? (
+              <img
+                onClick={() => setSelectedFile(null)}
+                src={imageFileUrl}
+                alt='selected file'
+                className={`w-full max-h-[250px] object-over cursor-pointer ${
+                  imageFileUploading ? 'animate-pulse' : ''
+                }`}
+              />
+            ) : (
+              <HiCamera
+                onClick={() => filePickerRef.current.click()}
+                className='text-5xl text-gray-400 cursor-pointer'
+              />
+            )}
+            <input
+              hidden
+              ref={filePickerRef}
+              type='file'
+              accept='image/*'
+              onChange={addImageToPost}
+            />
           </div>
           <input
             type='text'
@@ -89,8 +162,9 @@ export default function Header() {
           >
             Upload Post
           </button>
-          <AiOutlineClose className='cursor-pointer absolute top-2 right-2 hover:text-red-600 transition duration-300' 
-          onClick={() => setIsOpen(false)}
+          <AiOutlineClose
+            className='cursor-pointer absolute top-2 right-2 hover:text-red-600 transition duration-300'
+            onClick={() => setIsOpen(false)}
           />
         </Modal>
       )}
---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "insta-next-v2.firebaseapp.com",
+  projectId: "insta-next-v2",
+  storageBucket: "insta-next-v2.appspot.com",
+  messagingSenderId: "971986010195",
+  appId: "1:971986010195:web:6d48fe4db9a0c1817606a6"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
+// service firebase.storage {
+//     match /b/{bucket}/o {
+//       match /{allPaths=**} {
+//         allow read;
+//         allow write: if
+//         request.resource.size < 2 * 1024 * 1024 &&
+//         request.resource.contentType.matches('image/.*')
+//       }
+//     }
+//   }
